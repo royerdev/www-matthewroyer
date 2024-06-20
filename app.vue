04cb8c58e8545a5f987a857a076535ff145e7223
Matt Royer <20653437+matt-royer@users.noreply.github.com>
2024-06-20 06:08:22 -0400

    Init

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-initial">
+    <Skill name="JavaScript" :progress="80" badge />
+    <Skill name="Nuxt" :progress="80" badge />
+    <Skill name="CSS" :progress="100" badge />
+    <Skill name="XML" :progress="20" />
+  </div>
+</template>
---
+++ components/progressBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="progress-bar block w-full bg-gray-100 h-2.5">
+        <div class="bg-gray-500 h-2.5" :style="style"></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    progress: Number
+});
+
+const style = computed(() => {
+    return `width: ${props.progress}%`;
+});
+</script>
