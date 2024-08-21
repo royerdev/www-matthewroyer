1c5bab100c64a1e2c604ee599ffd8a34b62eb6bc
Matt Royer <20653437+matt-royer@users.noreply.github.com>
2024-08-21 12:48:10 -0400

    Removed sandbox components

--- app.vue
+++ app.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="flex flex-initial">
-    <Skill name="JavaScript" :progress="80" badge />
-    <Skill name="Nuxt" :progress="80" badge />
-    <Skill name="CSS" :progress="100" badge />
-    <Skill name="XML" :progress="20" />
+  <div>
+    <NuxtRouteAnnouncer />
   </div>
 </template>
