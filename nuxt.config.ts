6ba2111eac9d62eb4e1228cc1ce5c4ce73d04097
Matt Royer <20653437+matt-royer@users.noreply.github.com>
2024-08-21 12:51:37 -0400

    Added Nuxt UI and eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  modules: ['@nuxt/ui', '@nuxt/image', '@vueuse/nuxt', '@nuxt/eslint'],
+  app: {
+    head: {
+      title: 'Matt Royer',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+        { name: 'description', content: 'Matt Royer is an experienced full-stack software engineer and application designer based in the United States.' },
+      ],
+    }
+  },
+  nitro: {
+    compressPublicAssets: {
+      brotli: true
+    }
+  },
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  },
 })
---
+++ tailwind.config.ts
@@ -0,0 +1,41 @@
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'Myriad Pro', 'Calibri', 'sans-serif'],
+      },
+      colors: {
+        blue: {
+          50: '#f5f8fa',
+          100: '#eaeff4',
+          200: '#d0dde7',
+          300: '#a6c1d3',
+          400: '#769fba',
+          500: '#5483a3',
+          600: '#416a88',
+          700: '#36556e',
+          800: '#2f495d',
+          900: '#2b3f4f',
+          950: '#243341',
+          DEFAULT: '#243341',
+        },
+        accent: '#c8b293',
+      },
+    },
+    letterSpacing: {
+      'tighter': '-0.05em',
+      'tight': '0em',
+      'normal': '0.1em',
+      'wide': '0.15em',
+      'wider': '0.5em',
+      'widest': '0.75em',
+    },
+  },
+  darkMode: ['variant', [
+    '@media (prefers-color-scheme: dark) { &:not(.light *) }',
+    '&:is(.dark *)',
+  ]],
+}
