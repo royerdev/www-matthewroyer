ab544cfc77a032f623939af9e97b7d9fda668d96
Matt Royer <20653437+matt-royer@users.noreply.github.com>
2024-08-22 08:54:59 -0400

    Header and footer tweaks

--- components/Section/Footer.vue
+++ components/Section/Footer.vue
@@ -5,9 +5,7 @@
   >
     <p class="tracking-wide">
       &copy; {{ new Date().getFullYear() }}
-      &bullet; <nuxt-link to="/privacy">
-        Privacy
-      </nuxt-link>
+      &bullet; <nuxt-link to="/privacy">Privacy</nuxt-link>
       &bullet; Designed by <nuxt-link to="https://matthewroyer.com">
         Matt Royer
       </nuxt-link>
--- components/Section/Hero.vue
+++ components/Section/Hero.vue
@@ -1,16 +1,16 @@
 <template>
   <BaseSection
     centered
-    class="flex bg-blue-950 text-white sm:min-h-96 flex-col gap-1 self-end justify-end overflow-hidden relative p-8 sm:py-16"
+    class="flex bg-blue-950 text-white min-h-80 sm:min-h-96 flex-col gap-1 self-end justify-end overflow-hidden relative p-8 sm:py-16"
   >
     <div
-      class="hidden sm:block space-y-2 text-base leading-7 min-h mb-6 z-10"
+      class="space-y-2 text-base leading-7 min-h mb-6 z-10"
       :style="`opacity: ${opacity}`"
     >
-      <h1 class="mb-4 text-4xl sm:text-5xl md:text-6xl tracking-tight">
+      <h1 class="mb-4 text-3xl sm:text-5xl md:text-6xl tracking-tight">
         Software &amp; Production
       </h1>
-      <p class="text-2xl tracking-wider md:tracking-widest text-gray-400">
+      <p class="text-1xl sm:text-2xl tracking-wider md:tracking-widest text-gray-400">
         Georgia, USA
       </p>
     </div>
