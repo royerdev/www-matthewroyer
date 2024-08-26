ad6545125478bd862d363e6c2251f87f7b121bee
Matt Royer <20653437+matt-royer@users.noreply.github.com>
2024-08-25 21:10:00 -0400

    Added light/dark mode to popover

--- app/components/LinkedInBadge.vue
+++ app/components/LinkedInBadge.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pointer-events-none absolute bg-black text-gray-300 text-xs w-40 m-auto left-1/2 transform -translate-x-1/2 p-2 -bottom-14 -mb-1 opacity-0 transition-opacity group-hover:opacity-100 flex flex-row gap-2 justify-between items-center z-10 shadow text-left">
+  <div class="pointer-events-none absolute bg-white dark:bg-black text-gray-700 dark:text-gray-300 text-xs leading-3 w-40 m-auto left-1/2 transform -translate-x-1/2 p-3 -bottom-14 -mb-1 opacity-0 transition-opacity group-hover:opacity-100 flex flex-row gap-2 justify-between items-center z-10 shadow text-left">
     <div data-popper-arrow class="invisible absolute h-4 w-4 bg-inherit m-auto left-1/2 transform -translate-x-1/2 -top-2 before:visible before:absolute before:h-4 before:w-4 before:rotate-45 before:bg-inherit before:content-['']" />
     <svg
       data-tooltip-target="tooltip-default"
@@ -14,6 +14,6 @@
     >
       <path d="M14.73 10H17l-5.5 8L8 14.5l1.34-1.34L11.21 15zM20 3v16a3 3 0 01-3 3H7a3 3 0 01-3-3V3h5.69l.52-1A2 2 0 0112 1a2 2 0 011.76 1l.52 1zm-2 2h-2.6l.6 1.1V7H8v-.9L8.6 5H6v14a1 1 0 001 1h10a1 1 0 001-1z" />
     </svg>
-    Verified LinkedIn skill assessment badge
+    <p>Verified LinkedIn skill assessment badge</p>
   </div>
 </template>
