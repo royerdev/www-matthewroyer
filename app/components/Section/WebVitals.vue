654efb35760167c8b912557d9a0143c5084b783a
Matt Royer <20653437+matt-royer@users.noreply.github.com>
2024-08-25 21:54:02 -0400

    Text leading adjustment

--- app/components/Section/WebVitals.vue
+++ app/components/Section/WebVitals.vue
@@ -1,7 +1,7 @@
 <template>
   <BaseSection centered class="dark bg-blue-900 text-gray-600 dark:text-gray-300 text-xs">
     <LighthouseReport />
-    <p class="max-w-[450px] m-auto text-gray-300">
+    <p class="max-w-[450px] m-auto text-gray-300 leading-normal">
       This site was carefully designed for performance, accessibility, user experience, and SEO. Run a
       <NuxtLink to="https://pagespeed.web.dev/analysis/https-www-matthewroyer-com/b97wwjg91w?form_factor=desktop" target="_blank">scan</NuxtLink>
       to see for yourself.
