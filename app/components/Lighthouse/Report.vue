e08173eb28afe53b10689cb1f9179f0d79cf1cdd
Matt Royer <20653437+matt-royer@users.noreply.github.com>
2024-08-26 20:58:04 -0400

    Add confetti for fun

--- app/components/Lighthouse/Report.vue
+++ app/components/Lighthouse/Report.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="w-full flex items-center justify-center gap-16 sm:mb-8 sm:mt-2">
+  <div
+    class="w-full flex items-center justify-center gap-16 sm:mb-8 sm:mt-2"
+    ref="lighthouseReport"
+    @mouseenter="sendConfetti"
+  >
     <LighthouseScore
       v-for="(metric, index) in metrics"
       :key="index"
@@ -15,10 +19,47 @@
 </template>
 
 <script setup lang="ts">
+import confetti from 'canvas-confetti'
+
 const metrics = ref([
   { label: 'Performance', value: 100 },
   { label: 'Accessibility', value: 100 },
   { label: 'Best Practices', value: 100 },
   { label: 'SEO', value: 100 },
 ])
+
+const lighthouseReport = ref(null)
+const confettiCount = ref(0)
+
+onMounted(() => {
+  if (import.meta.client && lighthouseReport.value) {
+    const observer = new IntersectionObserver(onIntersection)
+    observer.observe(lighthouseReport.value)
+  }
+})
+
+const sendConfetti = () => {
+  confettiCount.value++
+  if (confettiCount.value > 10) return
+  const x = Math.random() * 0.4 + 0.3 // Random number between 0.3 and 0.7
+  confetti({
+    shapes: ['circle'],
+    scalar: 0.5,
+    origin: { x, y: 0.8 },
+  })
+}
+
+interface IntersectionObserverEntry {
+  isIntersecting: boolean;
+}
+
+const onIntersection = (entries: IntersectionObserverEntry[]) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Send it twice initially
+      sendConfetti()
+      sendConfetti()
+    }
+  })
+}
 </script>
